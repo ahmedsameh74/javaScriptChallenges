b21b7a8c814c353b8ca64af7a585e5f5e2f4891c
ahmed sameh <asameh1500@gmail.com>
2022-08-20 06:30:22 +0200

    challenges

---
+++ script.js
@@ -0,0 +1,42 @@
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
+
+// let firstName = 'Yasmen';
+// console.log(firstName);
+
+//firstChallengeSolution && secondChallengSolution🎉
+// let markHeight = 1.69;
+// let markMass = 78;
+// let johnHeight = 1.95;
+// let johnMass = 92;
+// let markBMI = markMass / markHeight ** 2;
+// let johnBMI = johnMass / johnHeight ** 2;
+// let markHeigherBMI = markBMI > johnBMI;
+// console.log(markBMI, johnBMI, markHeigherBMI);
+
+// if(markHeigherBMI){
+//     console.log(`Mark's BMI ${markBMI} is higher than John's ${johnBMI}`);
+// }else{
+//     console.log(`John's BMI ${johnBMI} is higher than Mark's ${markBMI}`);
+// }
+
+//thirdChallengeSolution🎉
+// const dolphines = (97 + 112 + 80) / 3;
+// const koalas = (109 + 95 + 36) / 3;
+// const min = 100;
+
+// console.log(dolphines, koalas);
+// if(dolphines > koalas && dolphines >= min){
+//     console.log('dolphies win!🏆');
+// }else if(dolphines < koalas && koalas >= min){
+//     console.log("koalas win!🏆");
+// }else if (dolphines === koalas && dolphines >= min && koalas >= min){
+//     console.log('draw!');
+// }else{
+//     console.log('no winner');
+// }
+
+//fourthChallengeSolution🎉
+// const bill = 40;
+// let tip = bill > 50 && bill < 300 ?  bill * 0.15 : bill * 0.20;
+// console.log(`the bill was ${bill}, the tip is ${tip}, and the total is ${bill + tip}`);
